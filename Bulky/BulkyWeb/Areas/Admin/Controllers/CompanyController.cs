0256b5ac08ef71466b32cef8b05b3b1f634e6c4b
DESKTOP-2M8R6NM\Momentuum <abhishek@momentuum.com>
2023-10-21 19:55:38 -0400

    Company - CRUD added

---
+++ Bulky/BulkyWeb/Areas/Admin/Controllers/CompanyController.cs
@@ -0,0 +1,82 @@
+﻿using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models;
+using Bulky.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    //[Authorize(Roles = SD.Role_Company)]
+    public class CompanyController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public CompanyController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Upsert(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return View(new Company());
+            }
+            else
+            {
+                Company company = _unitOfWork.Company.Get(x => x.Id == id);
+                return View(company);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Upsert(Company company)
+        {
+            if (ModelState.IsValid)
+            {
+                if (company.Id != 0)
+                {
+                    _unitOfWork.Company.Update(company);
+                    _unitOfWork.Save();
+                    TempData["Success"] = "Company updated successfully";
+                }
+                else
+                {
+                    _unitOfWork.Company.Add(company);
+                    _unitOfWork.Save();
+                    TempData["Success"] = "Company created successfully";
+                }
+                return RedirectToAction("Index");
+            }
+            return View(company);
+        }
+
+        #region API Calls
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<Company> companies = _unitOfWork.Company.GetAll().ToList();
+            return Json(new { data = companies });
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int? id)
+        {
+            Company? company = _unitOfWork.Company.Get(x => x.Id == id);
+            if (company == null)
+            {
+                return Json(new { success = false, message = "Error while deleting" });
+            }
+
+            _unitOfWork.Company.Remove(company);
+            _unitOfWork.Save();
+            return Json(new { success = true, message = "Company deleted successfully" });
+        }
+        #endregion
+    }
+}
---
+++ Bulky/BulkyWeb/wwwroot/js/company.js
@@ -0,0 +1,52 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#tblCompanies').DataTable({
+        "ajax": { url: '/admin/company/getall'},
+        "columns": [
+            { data: 'name', "width": "10%" },
+            { data: 'streetAddress', "width": "20%" },
+            { data: 'city', "width": "10%" },
+            { data: 'state', "width": "10%" },
+            { data: 'postalCode', "width": "15%" },
+            { data: 'phoneNumber', "width": "15%" },
+            {
+                data: 'id',
+                "render": function (data) {
+                    return `<div class='btn-group' role='group'>
+                        <a href='/admin/company/upsert?id=${data}' asp-route-id='@company.Id' class='btn btn-primary'><i class='bi bi-pencil-square'></i> Edit</a>
+                        <a onClick="Delete('/admin/company/delete?id=${data}')" asp-route-id='@company.Id' class='btn btn-danger'><i class='bi bi-trash'></i> Delete</a>
+                    </div>`;
+                },
+                "width": "20%"
+            }
+        ]
+    });
+}
+
+function Delete(url) {
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            $.ajax({
+                url: url,
+                type: "DELETE",
+                success: function (data) {
+                    dataTable.ajax.reload();
+                    toastr.success(data.message);
+                }
+            });
+        }
+    })
+}
