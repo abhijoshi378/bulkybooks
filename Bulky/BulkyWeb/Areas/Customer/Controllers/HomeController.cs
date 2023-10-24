f61704fb6c51487645596fa4299cdb5600ddeeae
DESKTOP-2M8R6NM\Momentuum <abhishek@momentuum.com>
2023-10-24 19:29:42 -0400

    Shopping Cart - Add to Cart enabled

--- Bulky/BulkyWeb/Areas/Customer/Controllers/HomeController.cs
+++ Bulky/BulkyWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
 using Bulky.DataAccess.Repository.IRepository;
 using Bulky.Models;
 using Bulky.Models.ViewModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Security.Claims;
 
 namespace BulkyWeb.Areas.Customer.Controllers
 {
@@ -32,8 +34,25 @@ namespace BulkyWeb.Areas.Customer.Controllers
             {
                 return NotFound();
             }
-            Product product = _unitOfWork.Product.Get(x => x.Id == id, includeProperties: "Category");
-            return View(product);
+            ShoppingCart shoppingCart = new ShoppingCart
+            {
+                Product = _unitOfWork.Product.Get(x => x.Id == id, includeProperties: "Category"),
+                Count = 1,
+                ProductId = id.Value
+            };
+            return View(shoppingCart);
+        }
+
+        [HttpPost]
+        [Authorize]
+        public IActionResult Details(ShoppingCart shoppingCart)
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+            shoppingCart.ApplicationUserId = userId;
+            _unitOfWork.ShoppingCart.Add(shoppingCart);
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
         }
 
         public IActionResult Privacy()
