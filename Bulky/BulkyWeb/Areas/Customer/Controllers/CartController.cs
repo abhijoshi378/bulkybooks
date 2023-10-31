d7c6abe10bda2c54d82343de32a626f1232d2393
DESKTOP-2M8R6NM\Momentuum <abhishek@momentuum.com>
2023-10-30 20:04:45 -0400

    Shopping Cart - Code added tocalculate quantity price and order total

--- Bulky/BulkyWeb/Areas/Customer/Controllers/CartController.cs
+++ Bulky/BulkyWeb/Areas/Customer/Controllers/CartController.cs
@@ -1,13 +1,58 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models;
+using Bulky.Models.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace BulkyWeb.Areas.Customer.Controllers
 {
     [Area("Customer")]
+    [Authorize]
     public class CartController : Controller
     {
+        private readonly ILogger<CartController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public ShoppingCartVM ShoppingCartVM { get; set; }
+        public CartController(ILogger<CartController> logger, IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment)
+        {
+            _logger = logger;
+            _unitOfWork = unitOfWork;
+            _webHostEnvironment = webHostEnvironment;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+            ShoppingCartVM = new ShoppingCartVM()
+            {
+                shoppingCartList = _unitOfWork.ShoppingCart.GetAll(x => x.ApplicationUserId == userId, includeProperties: "Product").ToList()
+            };
+
+            foreach(var c in ShoppingCartVM.shoppingCartList)
+            {
+                c.Price = GetQuantityBasedPrice(c);
+                ShoppingCartVM.OrderTotal = ShoppingCartVM.OrderTotal + (c.Price * c.Count);
+            }
+            return View(ShoppingCartVM);
+        }
+
+        private double GetQuantityBasedPrice(ShoppingCart shoppingCart)
+        {
+            if (shoppingCart.Count <= 50)
+            {
+                return shoppingCart.Product.Price;
+            }
+            else if (shoppingCart.Count <= 100)
+            {
+                return shoppingCart.Product.Price50;
+            }
+            else
+            {
+                return shoppingCart.Product.Price100;
+            }
         }
     }
 }
